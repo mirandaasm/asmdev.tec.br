5386f5ecea377a55cdfff8be3f3a6d9604b2f82b
Anderson Miranda <anderson@fastchannel.com>
2026-02-09 15:38:34 -0300

    Update app.vue with new template and script

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="site">
+    <div class="background-orbs"></div>
+
+    <div class="container is-fluid site-container">
+      <div class="columns is-variable is-6 is-multiline layout">
+        <div class="column is-4 profile-column">
+          <ProfileCard />
+          <NavMenu />
+        </div>
+
+        <div class="column is-8 content-column">
+          <main class="content-card">
+            <NuxtPage />
+          </main>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProfileCard from "@/components/ProfileCard.vue";
+import NavMenu from "@/components/NavMenu.vue";
+</script>
